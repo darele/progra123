7426b66a85f6f1470660abbed988cbfbd27ce346
darele <60158572+darele@users.noreply.github.com>
2020-07-23 21:56:51 -0600

    Create Tree Coloring.cpp

---
+++ CS Academy/Tree Coloring.cpp
@@ -0,0 +1,59 @@
+/*
+https://csacademy.com/contest/archive/task/tree-coloring/statement/
+*/
+
+#include <bits/stdc++.h>
+#define ll long long
+#define ii pair <int, int>
+#define dl pair <ll, ll>
+#define vi vector <int>
+#define vl vector <ll>
+#define vii vector <ii>
+#define graph vector <vi>
+#define mod 1000000007
+
+using namespace std;
+
+ll n, k;
+graph grafo;
+vi p, degree;
+
+ll dfs(int u, ll val) {
+    ll cans = val;
+    int cont = 1;
+    if (p[u]) cont++;
+    for (int v : grafo[u]) {
+        if (v == p[u]) continue;
+        p[v] = u;
+        cans *= dfs(v, k - cont);
+        cont++;
+        cans %= mod;
+    }
+    return cans;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    cin >> n >> k;
+    grafo.assign(n + 5, vi());
+    p.assign(n + 5, 0);
+    degree.assign(n + 5, 0);
+    int in1, in2;
+    for (int i = 1; i < n; i++) {
+        cin >> in1 >> in2;
+        grafo[in1].push_back(in2);
+        grafo[in2].push_back(in1);
+        degree[in1]++;
+        degree[in2]++;
+    }
+    int s;
+    for (int i = 0; i < n; i++) {
+        if (degree[i] == 1) {
+            s = i;
+            break;
+        }
+    }
+    cout << dfs(s, k) << "\n";
+	return 0;
+}
